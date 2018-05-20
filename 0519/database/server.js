53b4ddb0765dd3c7696b24860756330845653d37
Reumy <reuoomy@gmail.com>
2018-05-21 08:03:44 +0900

    Update server.js

--- 0519/database/server.js
+++ 0519/database/server.js
@@ -11,11 +11,11 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err){
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log('connect succes!')
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('connect succes!')
 });
  
 app.set('views', __dirname + '/views');
@@ -26,63 +26,62 @@ app.use(bodyParser.urlencoded({ extended : true }));
 app.listen(8080);
  
 app.get('/',(req,res)=>{
-	res.render('join');
+  res.render('join');
 })
 
 app.post('/register',(req,res)=>{
-	connection.query('SELECT * FROM users WHERE name="'+req.body.name+'"', function(err, rows){
-		if (err) {
-			console.log(err);
-			return;
-		}
-		if (rows.length > 0){
-			res.send('<script>alert("아이디가 이미 있음");location.href="/";</script>')
-
-		} else {
-			connection.query('INSERT INTO users SET ?', req.body, function (err, rows){
-				if (err) {
-	 				console.log(err);
-	 				return;
-	 			}
-	 			res.redirect('/login');
-			});
-		}
-	});
+  connection.query('SELECT * FROM users WHERE name="'+req.body.name+'"', function(err, rows){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (rows.length > 0){
+      res.send('<script>alert("아이디가 이미 있음");location.href="/";</script>')
+    } else {
+      connection.query('INSERT INTO users SET ?', req.body, function (err, rows){
+        if (err) {
+          console.log(err);
+          return;
+        }
+        res.redirect('/login');
+      });
+    }
+  });
 /*
-	connection.query('INSERT INTO users SET ?', req.body, function (err, rows){
-		if (err) {
-	 		console.log(err);
-	 		return;
-	 	}
-	 	res.redirect('/login');
-	});
+  connection.query('INSERT INTO users SET ?', req.body, function (err, rows){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect('/login');
+  });
 */
 });
  
 
 //로그인
 app.get('/login', (req, res)=>{
-	res.render('login');
+  res.render('login');
 });
 
 app.get('/main', (req, res)=>{
-	res.render('main');
+  res.render('main');
 });
 
 app.post('/check',(req,res)=>{
-	connection.query('SELECT * FROM users WHERE name="' + req.body.name + '"', function(err, rows){
-		if (err) {
-			console.log(err);
-			return;
-		}
-		if (rows.length > 0) {
-			if (rows[0].password == req.body.password) {
-				res.redirect('/main');
-			} else {
-				res.redirect('/login');
-			}
-		} else {
-			res.redirect('/');	
-		}
-	});
+  connection.query('SELECT * FROM users WHERE name="' + req.body.name + '"', function(err, rows){
+    if (err) {
+      console.log(err);
+      return;
+    }
+    if (rows.length > 0) {
+      if (rows[0].password == req.body.password) {
+        res.redirect('/main');
+      } else {
+        res.redirect('/login');
+      }
+    } else {
+      res.redirect('/');	
+    }
+  });
 });
