0dc92b920b29e1c0d1db7c3de9602cbc97b34986
Reumy <reuoomy@gmail.com>
2018-05-21 08:06:34 +0900

    Update Login.js

--- 0519/mvc/Login.js
+++ 0519/mvc/Login.js
@@ -1,11 +1,13 @@
 const Login = function(){
-		view : (req, res)=>{
-			res.render('/login');
-		}
-		register : (req, res)=>{
-			res.render('/register');
-		} 
-	}
+  return
+  {
+    view : (req, res)=>{
+      res.render('/login');
+    }
+    register : (req, res)=>{
+      res.render('/register');
+    } 
+  }
 }
 
 module.exports = new Login();
