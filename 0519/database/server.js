a6688ed93cf441955ddcbab512d883d7b371c2e2
Reumy <reuoomy@gmail.com>
2018-05-21 08:07:45 +0900

    Update server.js

--- 0519/database/server.js
+++ 0519/database/server.js
@@ -6,7 +6,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '0000',
+  password : '****',
   database : 'korea'
 });
 
