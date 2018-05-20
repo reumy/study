9ebbecfa18aba20e915ed15aaa272c2ad52925e8
Reumy <reuoomy@gmail.com>
2018-05-21 08:06:48 +0900

    Update router.js

--- 0519/mvc/router.js
+++ 0519/mvc/router.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const Router = app => {
-	const LoginController = require('./Login');
-	app.get('/',(req, res)=>LoginController.view(req, res));
-	app.get('/register',(req, res)=>LoginController.register(req, res));
+  const LoginController = require('./Login');
+  app.get('/',(req, res)=>LoginController.view(req, res));
+  app.get('/register',(req, res)=>LoginController.register(req, res));
 }
 
 module.exports = Router;
