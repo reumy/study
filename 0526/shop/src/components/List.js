c8d560696e0940ba59e2920e8f40da8119cbdb6f
Reumy <reuoomy@gmail.com>
2018-06-01 04:19:29 +0900

    Update List.js

--- 0526/shop/src/components/List.js
+++ 0526/shop/src/components/List.js
@@ -3,35 +3,35 @@ import Open from './Open';
 
 class List extends Component{
 
-	constructor(){
-		super();
-		this.state = {
-			num : 10,
-			show : false
-		}
-	}
+  constructor(){
+    super();
+    this.state = {
+      num : 10,
+      show : false
+    }
+  }
 
-	handleClick(){
-		this.setState({num:this.state.num + 1});
-	}
+  handleClick(){
+    this.setState({num:this.state.num + 1});
+  }
 
-	showClick(){
-		this.setState({show:!this.state.show});
-	}
+  showClick(){
+    this.setState({show:!this.state.show});
+  }
 
   render(){
-  	const {num} = this.state;
-  	return (
-  		<div>
-  			<h1>List 컴포넌트</h1>
-  			<div>List 컴포넌트가 만든 state 변수 num은 {num}</div>
-  			<button onClick={this.handleClick.bind(this)}>숫자증가</button>
-  			<button onClick={this.showClick.bind(this)}>토글버튼</button>		
-  			<hr />
-  			{(this.state.show) && <Open count={num} upCount={this.handleClick.bind(this)}/>}
-  		</div>
-  	)
-	}
+    const {num} = this.state;
+    return (
+      <div>
+        <h1>List 컴포넌트</h1>
+        <div>List 컴포넌트가 만든 state 변수 num은 {num}</div>
+        <button onClick={this.handleClick.bind(this)}>숫자증가</button>
+        <button onClick={this.showClick.bind(this)}>토글버튼</button>		
+        <hr />
+        {(this.state.show) && <Open count={num} upCount={this.handleClick.bind(this)}/>}
+      </div>
+    )
+  }
 }
 
 export default List;
