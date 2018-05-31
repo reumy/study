967de81a1aa3000225fb6b8a8dac0fc17144b6b3
Reumy <reuoomy@gmail.com>
2018-06-01 04:13:29 +0900

    Update Close.js

--- 0526/shop/src/components/Close.js
+++ 0526/shop/src/components/Close.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 
 class Close extends Component{
 
-  render(){
-  	const {count} = this.props;
-  	return (
-  		<div>
-  			<h1>Close 컴포넌트</h1>
-  			<div>상위 Open 컴포넌트가 전달해준 데이터는 {count}</div>
-  			<button onClick={this.props.upCount}>숫자증가</button>
-  		</div>
-  	)
-	}
+	render(){
+    const {count} = this.props;
+    return (
+      <div>
+        <h1>Close 컴포넌트</h1>
+        <div>상위 Open 컴포넌트가 전달해준 데이터는 {count}</div>
+        <button onClick={this.props.upCount}>숫자증가</button>
+      </div>
+    )
+  }
+	
 }
 
 export default Close;
