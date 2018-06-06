0b1498c85fd22d0088dd934ca29ea32581e73b24
Reumy <reuoomy@gmail.com>
2018-06-07 02:10:01 +0900

    Update App.js

--- 0602/four/src/App.js
+++ 0602/four/src/App.js
@@ -3,17 +3,17 @@ import InputForm from './InputForm';
 import ArrayHandle from './ArrayHandle';
 
 class App extends React.Component {
-	render(){
-		return (
-			<div>
-				<h1>리액트 입력폼 방식</h1>
-				<hr />
-				<InputForm />
-				<hr />
-				<ArrayHandle />
-			</div>
-		)
-	}
+  render(){
+    return (
+      <div>
+        <h1>리액트 입력폼 방식</h1>
+        <hr />
+        <InputForm />
+        <hr />
+        <ArrayHandle />
+      </div>
+    )
+  }
 }
 
 export default App;
