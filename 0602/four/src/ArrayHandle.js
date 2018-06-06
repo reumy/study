1e8522503f584ea91b4248860e9100605086789c
Reumy <reuoomy@gmail.com>
2018-06-07 02:13:05 +0900

    Update ArrayHandle.js

--- 0602/four/src/ArrayHandle.js
+++ 0602/four/src/ArrayHandle.js
@@ -2,20 +2,22 @@ import React from 'react';
 import List from './List';
 
 class ArrayHandle extends React.Component {
-	render (){
-		let num = [10, 20, 30, 40, 50]
-		let result = num.map((value, index)=><List key={index} info={value} />);
-		let result1 = num.filter((value, index)=>{
-			return value > 20;
-		});
-		return (
-			<div>
-				<h2>배열 다루기</h2>
-				<hr />
-				{result}
-			</div>
-		)
-	}
+	
+  render (){	  
+    let num = [10, 20, 30, 40, 50]
+    let result = num.map((value, index)=><List key={index} info={value} />);
+    let result1 = num.filter((value, index)=>{
+      return value > 20;
+    });
+	  
+    return (
+      <div>
+        <h2>배열 다루기</h2>
+        <hr />
+        {result}
+      </div>
+    )
+  }
 }
 
 export default ArrayHandle;
