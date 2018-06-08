06fe4a3e4a5cf3f469043b59b0b54db3a9706ae3
Reumy <reuoomy@gmail.com>
2018-06-09 02:14:44 +0900

    Update App.js

--- 0602/router/src/App.js
+++ 0602/router/src/App.js
@@ -6,23 +6,23 @@ import Cafe from './Cafe';
 import './App.css'
 
 class App extends React.Component {
-	render(){
-		console.log(this.props);
-		return (
-			<Router>
-				<div>
-					<h1>리액트 라우트</h1>
-					<NavLink exact to="/">메인으로 가기</NavLink><br />
-					<NavLink to="/blog/reumy">블로그로 가기</NavLink><br />
-					<NavLink to="/cafe">카페로 가기</NavLink><br />					
-					<hr />
-					<Route exact path="/" component={Main} />
-					<Route path="/blog/:title" component={Blog} />
-					<Route path="/cafe" component={Cafe} />
-				</div>
-			</Router>
-		)
-	}
+  render(){
+    console.log(this.props);
+    return (
+      <Router>
+        <div>
+          <h1>리액트 라우트</h1>
+          <NavLink exact to="/">메인으로 가기</NavLink><br />
+          <NavLink to="/blog/reumy">블로그로 가기</NavLink><br />
+          <NavLink to="/cafe">카페로 가기</NavLink><br />					
+          <hr />
+          <Route exact path="/" component={Main} />
+          <Route path="/blog/:title" component={Blog} />
+          <Route path="/cafe" component={Cafe} />
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
