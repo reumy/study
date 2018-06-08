0c5d884153b219ee5de41104dc61134d079d9663
Reumy <reuoomy@gmail.com>
2018-06-09 02:16:15 +0900

    Update Cafe.js

--- 0602/router/src/Cafe.js
+++ 0602/router/src/Cafe.js
@@ -3,18 +3,18 @@ import {Redirect} from 'react-router-dom';
 
 class Cafe extends React.Component {
 
-	state = {
-		logged : true
-	}
+  state = {
+    logged : true
+  }
 
-	render(){
-		return (
-			<div>
-				{!this.state.logged && <Redirect to="/" />}
-				<h2>카페 페이지</h2>
-			</div>
-		)
-	}
+  render(){
+    return (
+      <div>
+        {!this.state.logged && <Redirect to="/" />}
+        <h2>카페 페이지</h2>
+      </div>
+    )
+  }
 }
 
 export default Cafe;
