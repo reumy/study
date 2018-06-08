1f322bef809b77b8e0759e889cce1ddcf8b426df
Reumy <reuoomy@gmail.com>
2018-06-09 02:16:40 +0900

    Update Main.js

--- 0602/router/src/Main.js
+++ 0602/router/src/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Main extends React.Component {
-	render(){
-		return (
-			<div>
-				<h2>메인 페이지</h2>
-			</div>
-		)
-	}
+  render(){
+    return (
+      <div>
+        <h2>메인 페이지</h2>
+      </div>
+    )
+  }
 }
 
 export default Main;
