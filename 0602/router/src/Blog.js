8febe609561726f5e81e5567403b2452ea94836d
Reumy <reuoomy@gmail.com>
2018-06-09 02:15:33 +0900

    Update Blog.js

--- 0602/router/src/Blog.js
+++ 0602/router/src/Blog.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 class Blog extends React.Component {
 
-	handleClick(){
-		this.props.history.push('/');
-	}
+  handleClick(){
+    this.props.history.push('/');
+  }
 
-	render(){
-		console.log(this.props.match.params.title);
-		return (
-			<div>
-				<h2>{this.props.match.params.title}블로그 페이지</h2>
-				<button onClick={this.handleClick.bind(this)}>메인으로 돌아가기</button>
-			</div>
-		)
-	}
+  render(){
+    console.log(this.props.match.params.title);
+    return (
+      <div>
+        <h2>{this.props.match.params.title}블로그 페이지</h2>
+        <button onClick={this.handleClick.bind(this)}>메인으로 돌아가기</button>
+      </div>
+    )
+  }
 }
 
 export default Blog;
