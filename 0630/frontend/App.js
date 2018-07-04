f838b48fe77e060e7041c9f5a2f70953fd850b77
Reumy <reuoomy@gmail.com>
2018-07-04 12:01:54 +0900

    Update App.js

--- 0630/frontend/App.js
+++ 0630/frontend/App.js
@@ -23,24 +23,24 @@ class App extends Component {
   render() {
     return (
     	<Router>
-	      <div className="App">
-	        <h1>프론트엔드</h1>
-          <Link to="/">로그인</Link><br />
-          <Link to="/register">회원가입</Link><br />
-          <Link to="/phone">전화번호부</Link>
-	        <hr />
-	        <Route exact path="/" 
+          <div className="App">
+            <h1>프론트엔드</h1>
+            <Link to="/">로그인</Link><br />
+            <Link to="/register">회원가입</Link><br />
+            <Link to="/phone">전화번호부</Link>
+            <hr />
+            <Route exact path="/" 
               render={props=><Login {...props}
               status={this.state.logged}
               isLogin={this.isLogin.bind(this)} />} />
-          <Route exact path="/register" 
+            <Route exact path="/register" 
               render={props=><Register {...props}
               status={this.state.logged} />} />
-          <Route exact path="/phone" 
+            <Route exact path="/phone" 
               render={props=><Phone {...props}
               status={this.state.logged}
               isSelected={this.state.selectedID} />} />
-	      </div>
+        </div>
       </Router>
     );
   }
